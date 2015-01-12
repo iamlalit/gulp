4e474b6af0a7b03a0633b7ac6a97a5aa0e0601c0
lalit sachdeva <sachdeva_libra@yahoo.com>
2015-01-12 13:20:52 +0530

    updated for new updated code

--- app/pages/home/main.js
+++
@@ -1,8 +0,0 @@
-//bootstrap carousel image slider to slide the image
-$(document).ready(function(){
-    $('.carousel').carousel({
-      interval: false,
-      pause: ''
-    });
-    $('#contact-slider').off('keydown.bs.carousel');
-});
